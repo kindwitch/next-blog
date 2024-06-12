9186f57620917841f462ac11c589a82256fb7d66
Yiseul park <elane94@naver.com>
2024-06-12 11:35:15 +0900

    -1st

---
+++ src/app/intro/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Intropage() {
+  return (
+    <div className="container bg-slate-400">
+      <h1 className="text-2xl font-bold">Introduction</h1>
+      <p className="mt-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+        perspiciatis doloremque minima rem, enim deleniti assumenda molestias
+        corrupti ab quasi, ullam qui sequi. Quisquam temporibus fugiat
+        quibusdam, quis officia iste?
+      </p>
+    </div>
+  );
+}
